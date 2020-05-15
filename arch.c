469c3b328e50d617c815becc7ba75d0527334d89
Tomitom13 <60874830+Tomitom13@users.noreply.github.com>
2020-05-15 16:36:55 +0200

    Ajout des fonctiions mais pas le temps de détailler

--- arch.c
+++ arch.c
@@ -1,3 +1,16 @@
+
+
+//TODO  : La commande exit et resoudre le problème d'une commande en un mot
+//TODO 2: Résoudre le problème du dernier message quand quelqu'un se connecte
+//TODO 3: Faire la fonction !login et afficher le login dans le message !hello
+//TODO 4: Faire les options
+
+
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h> /* pour exit */
 #include <unistd.h> /* pour close */
@@ -11,8 +24,10 @@
 
 #define PORT IPPORT_USERRESERVED // = 5000
 #define MAX_USERS 5
-#define LG_MESSAGE   256
+#define LG_MESSAGE   500
 #define LENGHT_LOGIN 50
+
+#define VERSION "1.0"
 
 typedef struct User {
   int socketClient;
@@ -20,7 +35,13 @@ typedef struct User {
 }user;
 
 void initName(user users[MAX_USERS], int i);
-void detectCommande(char messageRecu[LG_MESSAGE]);
+void detectCommande(char messageRecu[LG_MESSAGE],  user *emetteur, user users[MAX_USERS]);
+void sendMessage(char destinataire[LENGHT_LOGIN], user *emetteur, char message[LG_MESSAGE], user users[MAX_USERS]);
+void version(int socket);
+void hello(int socket);
+void help(int socket);
+void list(int socket, user users[MAX_USERS]);
+
 
 
 int main()
@@ -83,105 +104,107 @@ int main()
     int nevents;
     int nfds = 0, qui = -1;
 
-    /*  for(int i =0; i<LENGHT_LOGIN; i++){
-    login_tampon[i]="";
-  }*/
-  // Liste des sockets à écouter
-  // socketEcoute + users[].socket => pollfds[]
-  pollfds[nfds].fd = socketEcoute;
-  pollfds[nfds].events = POLLIN;
-  pollfds[nfds].revents = 0;
-  nfds++;
 
-  for(int i = 0; i < MAX_USERS; i++) {
-    if(users[i].socketClient > 0) {
-      pollfds[nfds].fd = users[i].socketClient;
-      pollfds[nfds].events = POLLIN;
-      pollfds[nfds].revents = 0;
-      nfds++;
-    }
-  }
-  printf("avant le nevent\n");
-  nevents = poll(pollfds, nfds, -1);
-  if (nevents > 0) {
-    // parcours de pollfds[] à la recherche des revents != 0
-    for(int u = 0; u < nfds; u++) {
-      if(pollfds[u].revents != 0) {
-        if(u == 0) {
-          for(int i = 0; i < MAX_USERS; i++) {
-            if(users[i].socketClient == 0) {
-              users[i].socketClient = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, & longueurAdresse);
-              if (users[i].socketClient < 0) {
-                perror("accept");
-                close(users[i].socketClient);
-                close(socketEcoute);
-                exit(-4);
-              }
+    // Liste des sockets à écouter
+    // socketEcoute + users[].socket => pollfds[]
+    pollfds[nfds].fd = socketEcoute;
+    pollfds[nfds].events = POLLIN;
+    pollfds[nfds].revents = 0;
+    nfds++;
 
-
-              /*fonction bienvue
-              verion et !hello
-              */
-
-              /*
-              login par default
-              */
-              initName(users, i);
-
-
-
-
-              break;
-            }
-          }
-        }
-        else {
-          printf("else\n");
-          for(int i = 0; i < MAX_USERS; i++) {
-            if(pollfds[u].fd == users[i].socketClient) {
-              lus = read(users[i].socketClient, messageRecu, LG_MESSAGE*sizeof(char));
-
-              //fct detectCommande
-
-              detectCommande( messageRecu);
-
-              //Fin de la foncion
-
-
-
-
-              switch(lus) {
-                case -1 :
-                perror("read");
-                close(users[i].socketClient);
-                exit(-5);
-
-                case 0 :
-                fprintf(stderr, "La socket a été fermée par le client !\n\n");
-                close(users[i].socketClient);
-                users[i].socketClient = 0;
-                break;
-                default:
-                printf("Message envoyé par %s : %s (%d octets)\n\n",users[i].login, messageRecu, lus);
-              }
-            }
-          }
-        }
-        pollfds[u].revents = 0;
+    for(int i = 0; i < MAX_USERS; i++) {
+      if(users[i].socketClient > 0) {
+        pollfds[nfds].fd = users[i].socketClient;
+        pollfds[nfds].events = POLLIN;
+        pollfds[nfds].revents = 0;
+        nfds++;
       }
     }
-    // si c'est la socket socketEcoute => accept() + création d'une nouvelle entrée dans la table users[]
+    printf("avant le nevent\n");
+    nevents = poll(pollfds, nfds, -1);
+    if (nevents > 0) {
+      // parcours de pollfds[] à la recherche des revents != 0
+      for(int u = 0; u < nfds; u++) {
+        if(pollfds[u].revents != 0) {
+          if(u == 0) {
+            for(int i = 0; i < MAX_USERS; i++) {
+              if(users[i].socketClient == 0) {
+                users[i].socketClient = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, & longueurAdresse);
+                if (users[i].socketClient < 0) {
+                  perror("accept");
+                  close(users[i].socketClient);
+                  close(socketEcoute);
+                  exit(-4);
+                }
 
-    // sinon c'est une socket client => read() et gestion des erreurs pour le cas de la déconnexion
-  } else {
-    printf("poll() returned %d\n", nevents);
+
+                /*fonction bienvue
+                verion et !hello
+                */
+                hello(users[i].socketClient);
+                version(users[i].socketClient);
+
+
+
+                /*
+                login par default
+                */
+                initName(users, i);
+
+
+
+
+                break;
+              }
+            }
+          }
+          else {
+            printf("else\n");
+            for(int i = 0; i < MAX_USERS; i++) {
+              if(pollfds[u].fd == users[i].socketClient) {
+                lus = read(users[i].socketClient, messageRecu, LG_MESSAGE*sizeof(char));
+
+                //fct detectCommande
+
+                detectCommande(messageRecu, &users[i], users);
+
+                //Fin de la foncion
+
+
+
+
+                switch(lus) {
+                  case -1 :
+                  perror("read");
+                  close(users[i].socketClient);
+                  exit(-5);
+
+                  case 0 :
+                  fprintf(stderr, "La socket a été fermée par le client !\n\n");
+                  close(users[i].socketClient);
+                  users[i].socketClient = 0;
+                  break;
+                  default:
+                  printf("Message envoyé par %s : %s (%d octets)\n\n",users[i].login, messageRecu, lus);
+                }
+              }
+            }
+          }
+          pollfds[u].revents = 0;
+        }
+      }
+      // si c'est la socket socketEcoute => accept() + création d'une nouvelle entrée dans la table users[]
+
+      // sinon c'est une socket client => read() et gestion des erreurs pour le cas de la déconnexion
+    } else {
+      printf("poll() returned %d\n", nevents);
+    }
   }
-}
 
-// On ferme la ressource avant de quitter
-close(socketEcoute);
+  // On ferme la ressource avant de quitter
+  close(socketEcoute);
 
-return 0;
+  return 0;
 }
 
 
@@ -209,7 +232,7 @@ void initName(user users[MAX_USERS], int i){
 }
 
 
-void detectCommande(char messageRecu[LG_MESSAGE]){
+void detectCommande(char messageRecu[LG_MESSAGE], user *emetteur, user users[MAX_USERS]){
 
   int j;
   j = strcspn(messageRecu,"!");
@@ -222,19 +245,46 @@ void detectCommande(char messageRecu[LG_MESSAGE]){
     char messageCopy[LG_MESSAGE]; /* le message de la couche Application ! */
     char contenu[LG_MESSAGE] = ""; /* le message de la couche Application ! */
 
+    //pour message
+    char destinataire[LENGHT_LOGIN] = "";
+    char message[LG_MESSAGE] = "";
+
+
+    /*
+    *
+    *
+    *
+    *                       TO DO : Detetcer si il y a un seul mot. Car sans espaces (ex : !version), la commande n'est pas détectée
+    *
+    *
+    *
+    *
+    */
+
+
+
     strcpy(messageCopy,messageRecu);
     for (mot = strtok_r(messageCopy, " ", &rest);
     mot != NULL;
     mot = strtok_r(NULL, " ", &rest)) {
 
-
       switch(cpt){
         case 0:
         printf("commande : %s\n", mot);
-
-        if(strcmp(mot, "!message") == 0){
+        printf("messsage reçu  = |%s|\n",messageRecu);
+        if(strcmp(mot, "!msg") == 0){
           mode = 1;
         }
+        else if(strcmp(mot, "!version") == 0 || strcmp(messageRecu, "!version\n") == 0){
+          version(emetteur->socketClient);
+        }
+        else if(strcmp(mot, "!help") == 0 || strcmp(messageRecu, "!help\n") == 0){
+          help(emetteur->socketClient);
+        }
+        else if(strcmp(mot, "!list")== 0 || strcmp(messageRecu, "!list\n") == 0) {
+          list(emetteur->socketClient, users);
+        }
+
         else {
           printf("Commande non reconnue\n");
         }
@@ -243,7 +293,7 @@ void detectCommande(char messageRecu[LG_MESSAGE]){
         case 1:
         switch (mode) {
           case 1:
-          printf("destinataire : %s\n", mot);
+          strcat(destinataire, mot);
           break;
         }
         break;
@@ -251,6 +301,7 @@ void detectCommande(char messageRecu[LG_MESSAGE]){
         case 2:
         switch (mode) {
           case 1:
+
           strcat(contenu, mot);
           break;
         }
@@ -268,13 +319,99 @@ void detectCommande(char messageRecu[LG_MESSAGE]){
 
     }
     if(mode==1){
-      printf("message : |%s|", contenu);
 
-      printf("\n Fin du message \n\n");
+      sendMessage(destinataire, emetteur, contenu, users);
+
     }
     cpt=0;
 
   }
 
   j=-2;
+}
+
+
+void sendMessage(char destinataire[LENGHT_LOGIN], user *emetteur, char message[LG_MESSAGE], user users[MAX_USERS]){
+  /*
+
+  Forme du message a envoyer au client(s) concerné(es):  !msg login_from type le_message
+
+  Type = | (msg direct) ou & (msg broadcast)
+  */
+
+  char envoi[LG_MESSAGE]="!msg ";
+  strcat(envoi, emetteur->login);
+
+  if(strcmp(destinataire, "&") == 0){
+    strcat(envoi, " & ");
+  }
+  else{
+    strcat(envoi, " | ");
+  }
+
+  strcat(envoi, message);
+  printf("\nMessage trasmis à %s : %s\n", destinataire, envoi);
+
+  ///////////////////////////////// message groupé /////////////////////
+  if(strcmp(destinataire, "&") == 0){
+    for(int i=0; i<MAX_USERS; i++){
+      if(strcmp("", users[i].login)!=0){
+        printf("Envoyé à %s\n",users[i].login);
+        write(users[i].socketClient, envoi, strlen(envoi));  //TO DO : les erreurs, voir serveur TCP
+      }
+    }
+  }
+
+
+  ////////////////////////////// message perso /////////////////////////
+
+  for(int i=0; i<MAX_USERS; i++){
+    if(strcmp(destinataire, users[i].login)==0){
+      write(users[i].socketClient, envoi, strlen(envoi));  //TO DO : les erreurs, voir serveur TCP
+    }
+  }
+}
+
+void version(int socket){
+  char message[LG_MESSAGE]="!version ";
+
+  strcat(message, VERSION);
+
+  write(socket, message, strlen(message));  //TO DO : les erreurs, voir serveur TCP
+}
+
+void hello(int socket){
+  char message[LG_MESSAGE]="\t----------------------------------------\n\t| Salut, bienvenu(e) sur notre serveur |\n\t----------------------------------------\n\nPour prendre connaissances des possibilitées que vous avez, utilisez la commande : !help\n\n\n";
+
+  write(socket, message, strlen(message));  //TO DO : les erreurs, voir serveur TCP
+}
+
+void help(int socket){
+  char message[LG_MESSAGE]="Liste des commandes :\n\t !login mon_login : Se connecter et choisir son username (& interdit) \n\t !msg login le_message : Saisir le destinataire et le message qui lui est adressé \n\t !exit : Sortir du client\n\t !list : Affiche toutes les personnes connectées\n";
+
+  write(socket, message, strlen(message));  //TO DO : les erreurs, voir serveur TCP
+}
+
+void list(int socket, user users[MAX_USERS]){
+  char message[LG_MESSAGE] = "!list ";
+  int i=0;
+  printf("users[i].login = %s\n",users[i].login);
+  if(strcmp(users[i].login, "") != 0){
+    strcat(strcat(message,"&"), users[i].login);
+  }
+  i++;
+  for(i; i<MAX_USERS; i++){
+    printf("i =  %d\n",i);
+
+    printf("users[i].login = %s\n",users[i].login);
+
+    if(strcmp(users[i].login, "") != 0){
+      strcat(strcat(message,"&"), users[i].login);
+
+    }
+  }
+  printf("Fin\n");
+
+  write(socket, message, strlen(message));  //TO DO : les erreurs, voir serveur TCP
+
 }
